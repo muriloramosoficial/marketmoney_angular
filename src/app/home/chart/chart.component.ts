01c760e49d8de5af35f43bf639f6ce84e5e9da9b
Murilo Ramos Pires <murilorp@outlook.com>
2024-02-29 16:30:55 -0300

    initial commit update

--- src/app/home/chart/chart.component.ts
+++ src/app/home/chart/chart.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-chart',
+  standalone: true,
   templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.css']
+  styleUrls: ['./chart.component.css'],
 })
 export class ChartComponent implements OnInit {
-
-  constructor() { }
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
   ngOnInit() {
+    const script = this.renderer.createElement('script');
+    script.src =
+      'https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js';
+    script.async = true;
+    script.text = `
+    {
+      
+      "symbol": "NASDAQ:AAPL",
+      "interval": "D",
+      "timezone": "Etc/UTC",
+      "theme": "dark",
+      "style": "1",
+      "locale": "en",
+      "enable_publishing": false,
+      "allow_symbol_change": true,
+      "calendar": false,
+      "support_host": "https://www.tradingview.com"
+    }
+  `;
+    this.renderer.appendChild(this.el.nativeElement, script);
   }
-
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
 import { Header_menuComponent } from './header_menu/header_menu.component';
 import { Currency_selectorComponent } from './currency_selector/currency_selector.component';
+import { ChartComponent } from './chart/chart.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Header_menuComponent, Currency_selectorComponent],
+  imports: [Header_menuComponent, Currency_selectorComponent, ChartComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    const script = this.renderer.createElement('script');
+    script.src =
+      'https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js';
+    script.async = true;
+    script.text = `
+    {
+        "container_id": "basic-area-chart-demo",
+       
+        "autosize": true,
+        "symbol": "AAPL",
+        "interval": "D",
+        "timezone": "exchange",
+        "theme": "light",
+        "style": "3",
+        "hide_top_toolbar": true,
+        "save_image": false,
+        "locale": "en"
+    }
+  `;
+    this.renderer.appendChild(this.el.nativeElement, script);
+  }
 }
